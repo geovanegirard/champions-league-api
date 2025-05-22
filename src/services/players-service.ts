1a7815eb4276ec2fe8be9165342955430d2d280c
Geovane Girard <g.ng2009@hotmail.com>
2025-05-22 17:13:49 -0300

    implementing delete

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -40,3 +40,10 @@ export const createPlayerService = async (player: PlayerModel) =>{
 
     return response;
 };
+
+export const deletePlayerService = async (id: number) =>{
+    let response = null;
+    await PlayerRepository.deleteOnePlayer(id);
+    response = HttpResponse.ok({message: "deleted"});
+    return response;
+}
