dd7571aa30e515fc09eeb2c23bc80f4601b6a949
Geovane Girard <g.ng2009@hotmail.com>
2025-05-23 12:01:00 -0300

    implementing update, route, controller, service and repository

--- src/repositories/players-repository.ts
+++ src/repositories/players-repository.ts
@@ -1,4 +1,5 @@
 import { PlayerModel } from "../models/player-model";
+import { StatisticsModel } from "../models/statistics-model";
 
 const database: PlayerModel[] = [
   {
@@ -343,3 +344,13 @@ export const deleteOnePlayer = async (id: number) =>{
     database.splice(index, 1);
   }
 };
+
+export const findAndModifyPlayer = async (id: number, statistics: StatisticsModel): Promise<PlayerModel> => {
+  const playerIndex = database.findIndex( player => player.id === id)
+
+  if (playerIndex !== -1) {
+    database[playerIndex].statistics = statistics;
+  }
+
+  return database[playerIndex];
+}
--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,4 +1,5 @@
 import { PlayerModel } from "../models/player-model";
+import { StatisticsModel } from "../models/statistics-model";
 import * as PlayerRepository from "../repositories/players-repository";
 import * as HttpResponse from "../utils/http-helper";
 
@@ -15,35 +16,49 @@ export const getPlayerService = async () => {
     return response;
 };
 
-export const getPlayerByIdService = async (id: number) =>{
+export const getPlayerByIdService = async (id: number) => {
     const data = await PlayerRepository.findPlayerById(id);
     let response = null;
 
-    if (data){
-        response = HttpResponse.ok(data);
-    }else {
-        response = HttpResponse.noContent();
+    if (data) {
+        response = await HttpResponse.ok(data);
+    } else {
+        response = await HttpResponse.noContent();
     }
 
     return response;
 };
 
-export const createPlayerService = async (player: PlayerModel) =>{
+export const createPlayerService = async (player: PlayerModel) => {
     let response = null;
 
-    if (player){
+    if (player) {
         await PlayerRepository.insertPlayer(player);
-        response = HttpResponse.created();
-    }else{
-        response =  HttpResponse.badRequest();
+        response = await HttpResponse.created();
+    } else {
+        response = await HttpResponse.badRequest();
     }
 
     return response;
 };
 
-export const deletePlayerService = async (id: number) =>{
+export const deletePlayerService = async (id: number) => {
     let response = null;
     await PlayerRepository.deleteOnePlayer(id);
-    response = HttpResponse.ok({message: "deleted"});
+    response = await HttpResponse.ok({ message: "deleted" });
     return response;
 }
+
+export const updatePlayerService = async (id: number, statistics: StatisticsModel) => {
+    const data = await PlayerRepository.findAndModifyPlayer(id, statistics);
+    let response = null;
+
+    if (Object.keys(data).length ===0) {
+        response = await HttpResponse.badRequest();
+    }else {
+        response = await HttpResponse.ok(data);
+    }
+    
+    return response;
+
+};
